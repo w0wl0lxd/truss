f41485251df17e17bc6da8c08737c5ce059d0365
The truss Authors <truss@example.com>
2026-07-14 12:04:38 -0400

    build(nix): package truss with crane
    Expose packages.truss for installs while keeping the local devShell.

--- flake.nix
+++ flake.nix
@@ -4,6 +4,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
@@ -11,13 +12,51 @@
       self,
       nixpkgs,
       flake-utils,
+      crane,
     }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
         pkgs = import nixpkgs { inherit system; };
+        craneLib = crane.mkLib pkgs;
+
+        src = craneLib.cleanCargoSource ./.;
+
+        commonArgs = {
+          inherit src;
+          strictDeps = true;
+          buildInputs = [ ];
+          nativeBuildInputs = [
+            pkgs.pkg-config
+            pkgs.openssl
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        truss = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            pname = "truss";
+            cargoExtraArgs = "-p truss-cli";
+            doCheck = true;
+            cargoTestExtraArgs = "--workspace";
+          }
+        );
       in
       {
+        packages.default = truss;
+        packages.truss = truss;
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = truss;
+        };
+
+        checks = {
+          inherit truss;
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
             pkgs.rustup
@@ -29,10 +68,13 @@
             pkgs.pkg-config
             pkgs.openssl
             pkgs.protobuf
+            pkgs.gitleaks
+            pkgs.ripsecrets
           ];
 
           shellHook = ''
             eval "$(mise activate bash)"
+            git config core.hooksPath .githooks 2>/dev/null || true
           '';
         };
       }
